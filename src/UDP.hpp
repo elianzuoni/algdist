cfaeccbdd851c774aa261495347c04746c73ad41
Anzuoni Elia <elianzuoni@gmail.com>
2019-10-05 13:11:52 +0200

    First version of UDP.hpp

--- src/UDP.hpp
+++ src/UDP.hpp
@@ -1,1 +1,34 @@
-// wrapper for UDP sockets
+// UDP wrapper
+#pragma once
+
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+
+
+namespace udp
+{
+	/*An instance of this class wraps a UDP socket.
+	  Provides methods for sending and receiving messages with that socket.*/
+	class UDP
+	{
+		public:
+			/*Opens socket once and for all*/
+			UDP(char *srcIP, int srcPort);
+			/*Closes socket on destruction*/
+			~UDP();
+			
+			/*Sends message to specified recipient.
+			  Returns number of bytes sent, or -1.*/
+			ssize_t send(void *buf, size_t len, char *destIP, int destPort);
+			/*Blocks and listens for any incoming datagram.
+			  Saves sender's info in srcIP and srcPort.
+              Returns number of bytes received, or -1.*/
+			ssize_t receive(void *buf, size_t len, char *srcIP, int& srcPort);
+		
+		private:
+			/*The socket descriptor*/
+			int fd;
+	}
+}
